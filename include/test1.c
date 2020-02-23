842ea9b43242f702f78ba5f4bd66c71ec584fbfe
Ed Peguillan III <yankee14@protonmail.com>
2020-02-23 00:38:16 -0600

    Add placeholder content to test1

---
+++ include/test1.c
@@ -0,0 +1,7 @@
+#include "test1.h"
+
+uint8_t test1_func(void)
+{
+    test1_var = 0;
+    return test1_var;
+}
---
+++ include/test1.h
@@ -0,0 +1,10 @@
+#ifndef _TEST1_H_
+#define _TEST1_H_
+
+#include <stdint.h>
+
+uint8_t test1_var;
+
+uint8_t test1_func(void);
+
+#endif
